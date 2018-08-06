c7c2277dc02b29855688d3357eafec49730eb15b
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2018-08-07 01:52:08 +0600

    5/37 laravel create full project bangla part-4 Completed.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('student_login');
+});
+
+Route::get('/backend', function () {
+    return view('admin.admin_login');
 });
